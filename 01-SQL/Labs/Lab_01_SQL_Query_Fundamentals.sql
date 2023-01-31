51e5884768350e674686caf89c84d3115e56bc53
wloving77 <108347120+wloving77@users.noreply.github.com>
2023-01-31 12:08:46 -0500

    Final

--- 01-SQL/Labs/Lab_01_SQL_Query_Fundamentals.sql
+++ 01-SQL/Labs/Lab_01_SQL_Query_Fundamentals.sql
@@ -5,7 +5,9 @@ SELECT COUNT(*) AS Num_Rows FROM northwind.products;
 -- ------------------------------------------------------------------
 -- 1). Product Name and Unit/Quantity
 -- ------------------------------------------------------------------
-SELECT product_name, quantity_per_unit FROM northwind.products;
+SELECT product_name
+, quantity_per_unit 
+FROM northwind.products;
 -- ------------------------------------------------------------------
 -- 2). Product ID and Name of Current Products
 -- ------------------------------------------------------------------
@@ -45,7 +47,8 @@ SELECT id AS p_id
 , product_name AS p_name
 , list_price AS p_price 
 FROM northwind.products
-WHERE list_price < 20.00 AND list_price > 15.00;
+WHERE list_price <= 20.00 AND list_price >= 15.00
+ORDER BY list_price DESC;
 -- ------------------------------------------------------------------
 -- 7). Product Name & List Price Costing Above Average List Price
 -- ------------------------------------------------------------------
@@ -76,7 +79,8 @@ FROM northwind.products;
 -- 10). Product Name, Units on Order and Units in Stock
 --      Where Quantity In-Stock is Less Than the Quantity On-Order. 
 -- ------------------------------------------------------------------
-SELECT products.id, products.product_name
+SELECT products.id
+,products.product_name
 ,SUM(inventory_transactions.quantity) AS p_Stock
 ,SUM(purchase_order_details.quantity) AS p_On_Order
 FROM northwind.products
@@ -120,5 +124,4 @@ LEFT JOIN inventory_transactions ON inventory_transactions.id=products.id
 WHERE products.list_price < 20.00
 GROUP BY id;
 
-
-
+SELECT * FROM order_details;
